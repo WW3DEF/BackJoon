db039dca2d52e4e197edfa901d7ddbf5a09fac47
HwiJae Lee <zzangka1122@gmail.com>
2022-03-11 00:35:40 +0900

    입출력과 사칙연산

---
+++ src/step1.java
@@ -0,0 +1,15 @@
+
+public class step1 {
+	/*
+	 백준 문제를 풀시에 클래스명은 Main으로 해야 컴파일 에러가 발생하지 않음
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*
+		 print()는 내용을 출력
+		 println()은 한줄 띄워서 출력
+		 */
+		System.out.print("Hello World!");
+	}
+}
