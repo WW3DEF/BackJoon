2aa1a0a08f20a4bd5deb1f992f0c75b958f25187
HwiJae Lee <zzangka1122@gmail.com>
2022-03-20 22:50:46 +0900

    사칙연산

---
+++ src/step9.java
@@ -0,0 +1,23 @@
+import java.util.Iterator;
+import java.util.Random;
+import java.util.Scanner;
+
+public class step9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int A, B;
+		
+		A = sc.nextInt();
+		B = sc.nextInt();
+		
+		System.out.println(A+B);
+		System.out.println(A-B);
+		System.out.println(A*B);
+		System.out.println(A/B);
+		System.out.println(A%B);
+	}
+
+}
