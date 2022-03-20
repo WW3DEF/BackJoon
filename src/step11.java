d92c49eb1240d1e844a4ccc70cd22be9c143cbda
HwiJae Lee <zzangka1122@gmail.com>
2022-03-20 22:51:25 +0900

    불멸기원 계산법

---
+++ src/step11.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class step11 {
+	/*
+	 불멸기원은 석가모니가 열반한 해를 기준으로 삼는 연대 표기법이다.
+	 서기에서 544년을 더하면 불기의 연도가 된다. 일부 국가에선 543년을 더하면 된다.
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int A;
+		A = sc.nextInt();
+		
+		System.out.println(A-543);
+	}
+
+}
