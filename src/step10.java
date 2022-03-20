57b3c0f840299ced27ef185fea1c437f5f3f2c97
HwiJae Lee <zzangka1122@gmail.com>
2022-03-20 22:50:06 +0900

    아이디 선점을 빼앗긴 준하의 리액션 표현하기

---
+++ src/step10.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class step10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String A;
+		A = sc.next();
+		
+		System.out.println(A+"??!");
+	}
+
+}
