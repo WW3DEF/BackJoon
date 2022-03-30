2296b3f0802d85fccb80efa77df6684e03cd3bf2
HwiJae Lee <zzangka1122@gmail.com>
2022-03-30 17:17:58 +0900

    곱셈

--- src/step13.java
+++ src/step13.java
@@ -8,9 +8,9 @@ public class step13 {
 		int A,B;
 		A=sc.nextInt();
 		B=sc.nextInt();
-		System.out.println(A*(B%10));	// (B%10) 10의 자리 나눠서 남은 1의 자리를 A와 곱셈
-		System.out.println(A*(B%100));	// (B%100) 100의 자리를 나눠 남은 10의 자리와 1의 자리를 A와 곱셈
-		System.out.println(A*(B/100));	// (B/100) 100으로 나눠진 수만큼 A에 곱셈
+		System.out.println(A*(B%10));
+		System.out.println(A*(B%100/10));
+		System.out.println(A*(B/100));
 		System.out.println(A*B);
 	}
 
